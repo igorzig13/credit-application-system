0f5d21bb541f626641ff0edb675d100485df3cad
igorzig13 <igorzig13@gmail.com>
2024-02-09 22:41:24 -0300

    first commit

---
+++ src/test/kotlin/igorzig13/creditapplicationsystem/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package igorzig13.creditapplicationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
