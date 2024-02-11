db5b689e9df608ec1afc15556905dbbd83dc4e25
igorzig13 <igorzig13@gmail.com>
2024-02-11 08:24:23 -0300

    Cria as classes modelo da aplicação.

---
+++ src/main/kotlin/igorzig13/creditapplicationsystem/entity/Customer.kt
@@ -0,0 +1,34 @@
+package igorzig13.creditapplicationsystem.entity
+
+import jakarta.persistence.*
+
+@Entity
+//@Table(name = "customer")
+data class Customer(
+    @Column(nullable = false)
+    var firstName: String = "",
+
+    @Column(nullable = false)
+    var lastName: String = "",
+
+    @Column(nullable = false, unique = true)
+    val cpf: String,
+
+    @Column(nullable = false, unique = true)
+    var email: String,
+
+    @Column(nullable = false)
+    var password: String = "",
+
+    @Column(nullable = false) @Embedded
+    var address: Address = Address(),
+
+    @Column(nullable = false) @OneToMany(
+        fetch = FetchType.LAZY,
+        cascade = [(CascadeType.REMOVE)],
+        mappedBy = "customer")
+    val credits: List<Credit> = mutableListOf(),
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+)
