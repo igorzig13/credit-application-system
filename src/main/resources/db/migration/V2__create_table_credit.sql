1ce8eead7166b12f2a4eb3d951c82219683cedd9
igorzig13 <igorzig13@gmail.com>
2024-02-11 08:25:48 -0300

    Adiciona v1 e v2 nas migrações Flyway.

---
+++ src/main/resources/db/migration/V2__create_table_credit.sql
@@ -0,0 +1,17 @@
+CREATE TABLE credit
+(
+    id                     BIGINT AUTO_INCREMENT NOT NULL,
+    credit_code            BINARY(16)            NOT NULL,
+    credit_value           DECIMAL               NOT NULL,
+    day_first_installment  date                  NOT NULL,
+    number_of_installments INT                   NOT NULL,
+    status                 SMALLINT              NULL,
+    customer_id            BIGINT                NULL,
+    CONSTRAINT pk_credit PRIMARY KEY (id)
+);
+
+ALTER TABLE credit
+    ADD CONSTRAINT uc_credit_credit_code UNIQUE (credit_code);
+
+ALTER TABLE credit
+    ADD CONSTRAINT FK_CREDIT_ON_CUSTOMER FOREIGN KEY (customer_id) REFERENCES customer (id);
