c123cf81013ccba92b2445e5722eebc9005736b8
igorzig13 <igorzig13@gmail.com>
2024-02-12 09:43:14 -0300

    Cria funções de query na interface CreditRepository

--- src/main/kotlin/igorzig13/creditapplicationsystem/repository/CreditRepository.kt
+++ src/main/kotlin/igorzig13/creditapplicationsystem/repository/CreditRepository.kt
@@ -2,5 +2,12 @@ package igorzig13.creditapplicationsystem.repository
 
 import igorzig13.creditapplicationsystem.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.UUID
 
-interface CreditRepository: JpaRepository<Credit, Long>
+interface CreditRepository: JpaRepository<Credit, Long> {
+    fun findByCreditCode(creditCode: UUID): Credit?
+
+    @Query(value = "SELECT * FROM Credit WHERE customer_id = ?1", nativeQuery = true)
+    fun findAllByCustomer(customerId: Long): List<Credit>
+}
