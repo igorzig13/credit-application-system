2c521a3f3d82eec401863ae3cde682685c8995e9
igorzig13 <igorzig13@gmail.com>
2024-02-15 15:18:22 -0300

    Implementa validações aos DTOs.

--- src/main/kotlin/igorzig13/creditapplicationsystem/dto/CreditDto.kt
+++ src/main/kotlin/igorzig13/creditapplicationsystem/dto/CreditDto.kt
@@ -2,14 +2,17 @@ package igorzig13.creditapplicationsystem.dto
 
 import igorzig13.creditapplicationsystem.entity.Credit
 import igorzig13.creditapplicationsystem.entity.Customer
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.NotNull
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto(
-    val creditValue: BigDecimal,
-    val dayFirstInstallment: LocalDate,
-    val numberOfInstallments: Int,
-    val customerId: Long
+    @field:NotNull(message = "Invalid input") val creditValue: BigDecimal,
+    @field:Future(message = "Invalid date") val dayFirstInstallment: LocalDate,
+    @field:Max(value = 48, message = "Invalid input") val numberOfInstallments: Int,
+    @field:NotNull(message = "Invalid input") val customerId: Long
 ) {
     fun toEntity(): Credit = Credit(
         creditValue = this.creditValue,
--- src/main/kotlin/igorzig13/creditapplicationsystem/dto/CustomerDto.kt
+++ src/main/kotlin/igorzig13/creditapplicationsystem/dto/CustomerDto.kt
@@ -2,17 +2,23 @@ package igorzig13.creditapplicationsystem.dto
 
 import igorzig13.creditapplicationsystem.entity.Address
 import igorzig13.creditapplicationsystem.entity.Customer
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+import org.hibernate.validator.constraints.br.CPF
 import java.math.BigDecimal
 
 data class CustomerDto(
-    val firstName: String,
-    val lastName: String,
+    @field:NotEmpty(message = "Invalid input") val firstName: String,
+    @field:NotEmpty(message = "Invalid input") val lastName: String,
+    @field:NotEmpty(message = "Invalid input") @field:CPF(message = "Invalid CPF")
     val cpf: String,
-    val income: BigDecimal,
+    @field:NotNull(message = "Invalid input") val income: BigDecimal,
+    @field:NotEmpty(message = "Invalid input") @field:Email(message = "Invalid email")
     val email: String,
-    val password: String,
-    val zipCode: String,
-    val street: String
+    @field:NotEmpty(message = "Invalid input") val password: String,
+    @field:NotEmpty(message = "Invalid input") val zipCode: String,
+    @field:NotEmpty(message = "Invalid input") val street: String
 ) {
     fun toEntity(): Customer = Customer(
         firstName = this.firstName,
