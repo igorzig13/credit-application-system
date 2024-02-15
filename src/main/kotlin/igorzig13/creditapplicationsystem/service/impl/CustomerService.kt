b829ec5343c4b969906517818937266adabdf1fc
igorzig13 <igorzig13@gmail.com>
2024-02-15 15:20:12 -0300

    Reescreve função delete para remover via entidade.

--- src/main/kotlin/igorzig13/creditapplicationsystem/service/impl/CustomerService.kt
+++ src/main/kotlin/igorzig13/creditapplicationsystem/service/impl/CustomerService.kt
@@ -1,6 +1,7 @@
 package igorzig13.creditapplicationsystem.service.impl
 
 import igorzig13.creditapplicationsystem.entity.Customer
+import igorzig13.creditapplicationsystem.exception.BusinessException
 import igorzig13.creditapplicationsystem.repository.CustomerRepository
 import igorzig13.creditapplicationsystem.service.ICustomerService
 import org.springframework.stereotype.Service
@@ -13,11 +14,12 @@ class CustomerService(private val customerRepository: CustomerRepository
 
     override fun findById(id: Long): Customer =
         this.customerRepository.findById(id).orElseThrow {
-            throw RuntimeException("Id $id not found")
+            throw BusinessException("Id $id not found")
         }
 
     override fun delete(id: Long) {
-        this.customerRepository.deleteById(id)
+        val customer = this.findById(id)
+        this.customerRepository.delete(customer)
     }
 
 }
