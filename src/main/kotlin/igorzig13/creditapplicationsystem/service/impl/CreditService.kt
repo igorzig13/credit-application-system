e776df6181d95749b5bd4fc13553b5f943c2e1c3
igorzig13 <igorzig13@gmail.com>
2024-02-16 08:22:32 -0300

    Adiciona verificação de data da primeira parcela (fun validDayFirstInstallment)

--- src/main/kotlin/igorzig13/creditapplicationsystem/service/impl/CreditService.kt
+++ src/main/kotlin/igorzig13/creditapplicationsystem/service/impl/CreditService.kt
@@ -5,6 +5,7 @@ import igorzig13.creditapplicationsystem.exception.BusinessException
 import igorzig13.creditapplicationsystem.repository.CreditRepository
 import igorzig13.creditapplicationsystem.service.ICreditService
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.*
 
 @Service
@@ -13,6 +14,7 @@ class CreditService(
     private val customerService: CustomerService
 ): ICreditService {
     override fun save(credit: Credit): Credit {
+        this.validDayFirstInstallment(credit.dayFirstInstallment)
         credit.apply {
             customer = customerService.findById(credit.customer?.id!!)
         }
@@ -26,5 +28,10 @@ class CreditService(
         val credit: Credit = (this.creditRepository.findByCreditCode(creditCode)
             ?: throw BusinessException("Credit code $creditCode not found"))
         return if (credit.customer?.id == customerId) credit else throw IllegalArgumentException("Contact admin")
+    }
+
+    private fun validDayFirstInstallment(dayFirstInstallment: LocalDate): Boolean {
+        return if (dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3))) true
+        else throw BusinessException("Invalid Date")
     }
 }
