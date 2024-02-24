7d4ac598a56d155f8c12dde2d40c37f25eb0cdd1
igorzig13 <igorzig13@gmail.com>
2024-02-24 19:37:31 -0300

    Cria classe de teste para CustomerResource com testes para save e findById

---
+++ src/test/kotlin/igorzig13/creditapplicationsystem/controller/CustomerResourceTest.kt
@@ -0,0 +1,139 @@
+package igorzig13.creditapplicationsystem.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import igorzig13.creditapplicationsystem.dto.CustomerDto
+import igorzig13.creditapplicationsystem.entity.Customer
+import igorzig13.creditapplicationsystem.repository.CustomerRepository
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration
+class CustomerResourceTest {
+    @Autowired private lateinit var customerRepository: CustomerRepository
+    @Autowired private lateinit var mockMvc: MockMvc
+    @Autowired private lateinit var objectMapper: ObjectMapper
+
+    companion object {
+        const val URL: String = "/api/customers"
+    }
+
+    @BeforeEach fun setup() = customerRepository.deleteAll()
+    @AfterEach fun tearDown() = customerRepository.deleteAll()
+
+    @Test
+    fun `should create a customer and return 201 status`(){
+        //given
+        val customerDto: CustomerDto = buildCustomerDto()
+        val valueAsString = objectMapper.writeValueAsString(customerDto)
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Igor"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Marques"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value("121.133.094-08"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("igor@email.com"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("12345"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua de Igor"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `should not save a customer with same CPF and return 409 status`(){
+        //given
+        customerRepository.save(buildCustomerDto().toEntity())
+        val customerDto: CustomerDto = buildCustomerDto()
+        val valueAsString = objectMapper.writeValueAsString(customerDto)
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isConflict)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Conflict! Consult the documentation!"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(409))
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.exception")
+                    .value("class org.springframework.dao.DataIntegrityViolationException")
+            )
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `should find customer by id and return 200 status`(){
+        //given
+        val customer: Customer = customerRepository.save(buildCustomerDto().toEntity())
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("$URL/${customer.id}")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Igor"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Marques"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value("121.133.094-08"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("igor@email.com"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("12345"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua de Igor"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `should not find customer with invalid id and return 400 status`(){
+        //given
+        val invalidId: Long = -1L
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("$URL/$invalidId")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation!"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(400))
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.exception")
+                    .value("class igorzig13.creditapplicationsystem.exception.BusinessException")
+            )
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    private fun buildCustomerDto(
+        firstName: String = "Igor",
+        lastName: String = "Marques",
+        cpf: String = "121.133.094-08",
+        income: BigDecimal = BigDecimal.valueOf(1500.0),
+        email: String = "igor@email.com",
+        password: String = "abc123",
+        zipCode: String = "12345",
+        street: String = "Rua de Igor"
+    ) = CustomerDto(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        income = income,
+        email = email,
+        password = password,
+        zipCode = zipCode,
+        street = street
+
+    )
+}
